3d1731893349d722ba2ecb063f3fc9c999bddbc4
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-03-17 20:09:29 -0400

    User homepage basic info finished

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -1,17 +1,88 @@
 <script setup>
 import LoginRequireBox from '@/components/LoginRequireBox.vue'
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+import { Avatar, Button, message } from 'ant-design-vue';
+import axios from 'axios';
 
 const isLoggedIn = computed(()=>{
     if (localStorage.getItem("MEMESA_TOKEN") == null){
-        return true
-    }
-    else{
         return false
     }
+    else{
+        return true
+    }
 })
+
+const username = ref("未登录")
+const email = ref("未知邮箱")
+const userid = ref("未知UID")
+const description = ref("这个人很懒，什么也没写……")
+const password = ref("no_password")
+
+
+function gatherUserInfo(){
+    // send request to get user information from the db
+    let userToken = localStorage.getItem("MEMESA_TOKEN")
+    console.log(userToken)
+    axios({
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": userToken
+        },
+        method: "post",
+        url: "/user/getUserInfo",
+    }).then(data => {
+        console.log(data)
+        username.value = data.data.Data.username
+        password.value = data.data.Data.password
+        email.value = data.data.Data.email
+        userid.value = data.data.Data.id
+        description.value = data.data.Data.description
+    }).catch(err => {
+        console.log(err)
+        message.error("啊哦！我们在获取你的账号信息时出现了一些问题QAQ")
+    })
+}
+window.onload = gatherUserInfo
 </script>
 <template>
     <h1 class="page-title">我</h1>
-    <LoginRequireBox v-show="isLoggedIn"/>
+    <LoginRequireBox v-show="!isLoggedIn"/>
+    <div v-if="isLoggedIn">
+        <div class="static-content-block">
+            <div id="inline-display">
+                <Avatar :size="60">用户</Avatar>
+                <span class="inline-title">
+                    <h1 style="width: fit-content;">{{ username }}</h1>
+                    <div style="width: fit-content;">{{ description }}</div>
+                </span>
+                <Button type="primary">修改我的个人资料</Button>
+            </div>
+            <div>
+                <b>关注数：</b> | 
+                <b>粉丝数：</b> |
+                <b>稿件数：</b> |
+            </div>
+            
+        </div>
+    </div>
 </template>
+<style>
+.static-content-block{
+    border-radius: 5px;
+    margin: 5px;
+    padding: 15px;
+    width: auto;
+    height: fit-content;
+    background-color: #f9f9f9;
+}
+#inline-display{
+    display: flex;
+    align-items: center;
+}
+.inline-title{
+    padding-left: 10px;
+    padding-bottom: 10px;
+    width: 90%;
+}
+</style>
