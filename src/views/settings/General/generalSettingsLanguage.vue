55646c9e4f148c820d0c1d8a54b83d9728197e56
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-04-07 21:16:41 -0400

    Translate Update

--- src/views/settings/General/generalSettingsLanguage.vue
+++ src/views/settings/General/generalSettingsLanguage.vue
@@ -24,10 +24,6 @@ function initializeSelection(){
 initializeSelection()
 </script>
 <template>
-    <h2>语言与地区 | Language And Region</h2>
-    在这里选择您的语言偏好和显示语言。<br>
-    You could choose your region preferences and display language here. <br>
-    <Divider/>
     <h2>设置显示语言 Set up display language</h2>
     当前语言 (Current Language)：
     <Select style="width:200px;" v-model:value="selectedLanguageCode" @change="changeLanguage">
@@ -36,7 +32,4 @@ initializeSelection()
         <SelectOption value="zh_tw">繁體中文</SelectOption>
         <SelectOption value="jp_jp">日本語</SelectOption>
     </Select>
-    <br>
-    {{ $t('test.greetings') }}
-    <Button type="primary">应用</Button>
 </template>
