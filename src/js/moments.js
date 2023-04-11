00eb705f1d79eff72883fdb9d6490c301be37f65
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-04-11 14:22:13 -0400

    Moments Request JS File Building

---
+++ src/js/moments.js
@@ -0,0 +1,105 @@
+import axios from "axios"
+
+
+function generateAccessToken(){
+    return new Promise((res, rej) => {
+        let targetToken
+        axios({
+            method: "post",
+            url: "/imgAPI/token?username="+username+"&password="+password,
+        }).then(data => {
+            // get status
+            if (data.data.success != true){
+                targetToken = "false"
+                console.log(data)
+                console.log("Request success but error occured while API processing")
+                res(null)
+            }
+            else {
+                targetToken = data.data.data.token
+                console.log(targetToken)
+                console.log("API Token gathered successfully")
+                res(targetToken)
+            }
+        }).catch (err => {
+            console.log(err)
+            targetToken = "false"
+            res(null)
+        })
+    })
+}
+
+async function uploadMoment(fileList){
+    let accessToken = await generateAccessToken()
+    return new Promise((res, rej) => {
+        // Generate form data
+        let formDataList = []
+        for (let i = 0; i < fileList.length; i++){
+            let targetFormData = new FormData()
+            targetFormData.append("smfile", fileList[i])
+            targetFormData.append("format", "json")
+            formDataList.push(targetFormData)
+        }
+        // Start Upload the images to the image server
+        let returnList = []
+        for (let i = 0; i < formDataList.length; i++){
+            axios({
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": accessToken,
+                },
+                method: "post",
+                url: "/imgAPI/upload",
+                data: formDataList[i],
+            }).then(data => {
+                if (data.data.success != true && data.data.code != 'image_repeated'){
+                    res({status: false, data: []})
+                }
+                // collect the url and append to the return list
+                if (data.data.code == 'image_repeated'){
+                    returnList.push(data.data.images)
+                }
+                else {
+                    returnList.push(data.data.data.url)
+                }
+            }).catch(err => {
+                console.log(err)
+                res({status: false, data: []})
+            })
+        }
+        // finish all the steps, return data
+        res({status: true, data: returnList})
+    })
+
+}
+
+async function uploadMomentToDatabase(content, urlList, isPublic){
+    return new Promise((res, rej) => {
+        let targetData = {
+            "content": content,
+            "images": urlList,
+            "public": isPublic
+        }
+        axios({
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": localStorage.getItem("MEMESA_TOKEN")
+            },
+            method: "post",
+            url: "/api/moments/add",
+        }).then(data => {
+            if (data.data.Code != 200){
+                res(false)
+            }
+            res(true)
+        }).catch(err => {
+            console.log(err)
+            res(false)
+        })
+    })
+}
+
+export default{
+    uploadMoment,
+    uploadMomentToDatabase
+}
--- src/views/moments/myMoments.vue
+++ src/views/moments/myMoments.vue
@@ -1,8 +1,27 @@
 <script setup>
-import { Button, Result, Drawer, Input, Textarea, Upload } from 'ant-design-vue';
+import { Button, Result, Drawer, Input, Textarea, Upload, Switch, Tooltip, message } from 'ant-design-vue';
 import { ref } from 'vue';
 
 const uploadDrawerVisible = ref(false)
+const submitPublicPost = ref(true)
+const fileList = ref([])
+
+function beforeUpload(file){
+    // check whether the file could fulfill the upload requirement
+    if (file.type != 'image/*'){
+        fileList.value.splice(fileList.value.indexOf(file), 1)
+        message.error("目前只能上传图片哦QAQ")
+    }
+    if (!(file.size / 1024 / 1024 <= 2)){
+        fileList.value.splice(fileList.value.indexOf(file), 1)
+        message.error("文件太大啦！")
+    }
+}
+
+function uploadMoments(){
+    
+}
+
 </script>
 <template>
     <Button type="primary" style="margin-top: 5px;" @click="uploadDrawerVisible = !uploadDrawerVisible">发布新动态</Button>
@@ -15,9 +34,16 @@ const uploadDrawerVisible = ref(false)
         <h2>发布新动态</h2>
         完成后点击右上角发布即可
         <Textarea :bordered="false" placeholder="说些什么吧" style="margin-top: 5px;"></Textarea>
-        <Upload list-type="picture-card">
-            <img src="@/assets/plus-circle.svg">
-            <div style="margin-top: 8px;">上传最多4个图片</div>
+        <Upload list-type="picture-card" :max-count="4" v-model:file-list="fileList" :before-upload="beforeUpload">
+            <img src="@/assets/plus-circle.svg" style="display: inline-block;">
+            <!-- <div style="margin-top: 8px; display: inline-block;">上传最多4个图片</div> -->
         </Upload>
+        <Tooltip>
+            <template #title>
+                公开的动态可以被社区内所有的人看到。若不希望请关闭此选项！
+            </template>
+            这个动态是公开的
+        </Tooltip>
+        <Switch v-model:checked="submitPublicPost"/>
     </Drawer>
 </template>
